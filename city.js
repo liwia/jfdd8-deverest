0a5bf9fad585df624a65f60785215178630adf31
Jakub Patzer <nanonoise@gmail.com>
2017-12-01 17:02:18 +0100

    para v3

--- city.js
+++ city.js
@@ -1,7 +1,21 @@
 $(function () {
     var elements = Array.from({ length: 4}, function (_, index) {
         index ++;
-        return $('<div>').addClass('par' + index).css('left', (index-1) ).animate({ left: 1000 + ((index-1)  + 200)})
+
+        var result = $('<div>')
+            .addClass('par' + index)
+            ;
+
+        function loop() {
+          result.css('left', -100 ).animate({
+                left: 1000 + ((index-1)  + 200)
+            }, 2000 * index, 'linear', loop);
+        }
+
+        loop()
+
+        return result;
+
     })
     $('.horizontal-parallax').append(elements);
 });
