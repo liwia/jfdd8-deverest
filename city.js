5c686391b8c2709fc8d164e7d00207bca40f9ae9
Jakub Patzer <nanonoise@gmail.com>
2017-12-01 16:07:49 +0100

    parallax jquery v1

--- city.js
+++ city.js
@@ -1,6 +1,23 @@
 $(function () {
+
   $('.par1').each(function (index) {
-      $(this).css('left', index * 150)
+      $(this).css('left', index * 150);
       $(this).animate({ left: 1000 + (index * 150 + 200)})
-  })
+  });
+
+
+    $('.par2').each(function (index) {
+        $(this).css('left', index * 150);
+        $(this).animate({ left: 1000 + (index * 150 + 200)})
+    });
+
+    $('.par3').each(function (index) {
+        $(this).css('left', index * 150);
+        $(this).animate({ left: 1000 + (index * 150 + 200)})
+    });
+
+    $('.par4').each(function (index) {
+        $(this).css('left', index * 150);
+        $(this).animate({ left: 1000 + (index * 150 + 200)})
+    });
 });
