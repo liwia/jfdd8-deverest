d92ba30d8add9e376b2991bc4b365a17f6253a38
Jakub Patzer <nanonoise@gmail.com>
2017-12-01 16:47:46 +0100

    para v2

--- city.js
+++ city.js
@@ -1,7 +1,7 @@
 $(function () {
     var elements = Array.from({ length: 4}, function (_, index) {
         index ++;
-        return $('<div>').addClass('par' + index).css('left', (index-1) * 150).animate({ left: 1000 + ((index-1) * 150 + 200)})
+        return $('<div>').addClass('par' + index).css('left', (index-1) ).animate({ left: 1000 + ((index-1)  + 200)})
     })
     $('.horizontal-parallax').append(elements);
 });
