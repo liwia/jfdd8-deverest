2a6cce39ad1b2e9c54adcb535831636ec6a43642
Jakub Patzer <nanonoise@gmail.com>
2017-12-04 17:05:43 +0100

    para v5

--- city.js
+++ city.js
@@ -1,21 +1,33 @@
 $(function () {
+
+    var colors = ['#555555', '#666666', '#777777', '#888888'];
+
     var elements = Array.from({ length: 4}, function (_, index) {
         index ++;
 
+        var blocks = Array.from({ length: 550}, function (_, index2) {
+            return $('<div>').css({
+                height: Math.random() * 300,
+
+                backgroundColor: colors[index -1],
+                flex: 1
+            })
+        });
+
         var result = $('<div>')
-            .addClass('par' + index)
+            .addClass('par' + index).css('width', '10000%').css('display', 'flex').css('align-items', 'flex-end').append(blocks)
             ;
 
         function loop() {
-          result.css('left', -100 ).animate({
+          result.css('left', '-5000%' ).stop().animate({
                 left: 1100 + ((index-1)  + 200)
-            }, 2000 * index, 'linear', loop);
+            }, 4000 * (index + 1), 'linear', loop);
         }
 
-        loop()
+        loop();
 
         return result;
 
-    })
+    });
     $('.horizontal-parallax').append(elements);
 });
