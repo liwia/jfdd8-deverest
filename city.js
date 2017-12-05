4a374ac92ddd5081fe8d38912a120e54f15227e4
Jakub Patzer <nanonoise@gmail.com>
2017-12-05 15:58:29 +0100

    parallax FINAL!!!

--- city.js
+++ city.js
@@ -1,5 +1,8 @@
 $(function () {
 
+
+    var container = $('.horizontal-parallax');
+
     var colors = ['#555555', '#666666', '#777777', '#888888'];
 
     var elements = Array.from({ length: 4}, function (_, index) {
@@ -8,26 +11,26 @@ $(function () {
         var blocks = Array.from({ length: 550}, function (_, index2) {
             return $('<div>').css({
                 height: Math.random() * 300,
-
                 backgroundColor: colors[index -1],
                 flex: 1
             })
         });
 
         var result = $('<div>')
-            .addClass('par' + index).css('width', '5000%').css('display', 'flex').css('align-items', 'flex-end').append(blocks)
+            .addClass('par' + index).css('width', container.width() * 50).css('display', 'flex').css('align-items', 'flex-end').append(blocks)
             ;
 
         function loop() {
-          result.css('left', '-5000%' ).stop().animate({
-                left: 1100 + ((index-1)  + 200)
-            }, 10000 * (index + 1), 'linear', loop);
+            // debugger
+          result.stop().css('left', -container.width() * 50).animate({
+                left: 1300 + ((index-1))
+            }, 90000 * (index + 1), 'linear', loop)
         }
-
         loop();
+
 
         return result;
 
     });
-    $('.horizontal-parallax').append(elements);
+    container.append(elements);
 });
