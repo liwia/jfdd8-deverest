e2cb3e04c49c1e03217768237dabfd6e7893600d
Robert Cisek <r.cisek@o2.pl>
2017-12-06 16:56:43 +0100

    gra

--- game-test/init.js
+++ game-test/init.js
@@ -1,20 +1,34 @@
 var scoreNum = 0;
 
 $(document).ready(function () {
+    var bubble = $('.bubble');
     $('.startGame').click(function () {
-       scoreNum = 0;
-        $('.bubble').css('visibility', 'visible');
+        scoreNum = 0;
+        bubble.css('visibility', 'visible');
         $('.score').text(scoreNum);
     })
 
-    $('.bubble').click(function() {
-        $('.score').text(scoreNum +=10);
-        $('.bubble').css({
-            top: Math.random() * 300 + 500,
-            left: Math.random() * 300 + 500
+
+    var stopInterval = setInterval(function(){
+        var maxHeight = bubble.parent().height() - bubble.height();
+        var maxWidth = bubble.parent().width() - bubble.width();
+        bubble.css({
+            top: Math.random() * maxHeight,
+            left: Math.random() * maxWidth
         });
+    }, 1000);
+
+    bubble.click(function() {
+        $('.score').text(scoreNum +=10);
+
+
+        // $('.bubble').css({
+        //     top: Math.random() * 300 + 500,
+        //     left: Math.random() * 900 + 350
+        // });
+
         if (scoreNum === 1000) {
-            $('.bubble').css('visibility', 'hidden');
+            bubble.css('visibility', 'hidden');
             $('.toWin').text('You Win!').css({'opacity': '1', 'z-index': '0'});
             $('.startgame').text('Play Again?').css({'opacity': '1', 'z-index': '0'});
         }
