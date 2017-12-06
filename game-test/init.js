a709965ac5a5470551c6924667117086a98fe1a3
Rafał Penkala <raf.tricity@gmail.com>
2017-12-06 16:03:44 +0100

    game v 1.0.2 + timer poczatek v1.1

--- game-test/init.js
+++ game-test/init.js
@@ -2,18 +2,30 @@ var scoreNum = 0;
 
 $(document).ready(function () {
     $('.startGame').click(function () {
-       scoreNum = 0;
+        scoreNum = 0;
+        $('.scoreBoard').css('visibility', 'visible');
+        $('.timeBoard').css('visibility', 'visible');
         $('.bubble').css('visibility', 'visible');
         $('.score').text(scoreNum);
-    })
+        $('.timer').text(time);
+    });
 
-    $('.bubble').click(function() {
-        $('.score').text(scoreNum +=10);
+    var time = 30;
+    $('.timer').text(time);
+    var interval = setInterval(function () {
+        time--;
+        if (time === 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
+
+    $('.bubble').click(function () {
+        $('.score').text(scoreNum += 10);
         $('.bubble').css({
             top: Math.random() * 300 + 500,
             left: Math.random() * 300 + 500
         });
-        if (scoreNum === 1000) {
+        if (scoreNum === 100) {
             $('.bubble').css('visibility', 'hidden');
             $('.toWin').text('You Win!').css({'opacity': '1', 'z-index': '0'});
             $('.startgame').text('Play Again?').css({'opacity': '1', 'z-index': '0'});
