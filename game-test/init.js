9356c2e64f11016de6b76561e0c22017d83f5245
Rafał Penkala <raf.tricity@gmail.com>
2017-12-06 16:20:10 +0100

    game v 1.0.2 + timer fixeddd

--- game-test/init.js
+++ game-test/init.js
@@ -8,15 +8,17 @@ $(document).ready(function () {
         $('.bubble').css('visibility', 'visible');
         $('.score').text(scoreNum);
         $('.timer').text(time);
+
+        var time = 30;
+        var interval = setInterval(function () {
+            $('.timer').text(time--);
+            if (time === 0) {
+                clearInterval(interval);
+            }
+        }, 1000);
     });
 
-    var time = 30;
-    var interval = setInterval(function () {
-        $('.timer').text(time--);
-        if (time === 0) {
-            clearInterval(interval);
-        }
-    }, 1000);
+
 
     $('.bubble').click(function () {
         $('.score').text(scoreNum += 10);
