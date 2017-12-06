17aa12c2736942b37319eda90526b46f0bbfa5f8
Rafał Penkala <raf.tricity@gmail.com>
2017-12-06 16:49:42 +0100

    game v 1.0.3 + timer "Play again?"

--- game-test/init.js
+++ game-test/init.js
@@ -7,18 +7,16 @@ $(document).ready(function () {
         $('.timeBoard').css('visibility', 'visible');
         $('.bubble').css('visibility', 'visible');
         $('.score').text(scoreNum);
-        $('.timer').text(time);
 
         var time = 30;
         var interval = setInterval(function () {
-            $('.timer').text(time--);
             if (time === 0) {
                 clearInterval(interval);
             }
+            $('.timer').text(time--)
+            ;
         }, 1000);
     });
-
-
 
     $('.bubble').click(function () {
         $('.score').text(scoreNum += 10);
@@ -29,7 +27,7 @@ $(document).ready(function () {
         if (scoreNum === 100) {
             $('.bubble').css('visibility', 'hidden');
             $('.toWin').text('You Win!').css({'opacity': '1', 'z-index': '0'});
-            $('.startgame').text('Play Again?').css({'opacity': '1', 'z-index': '0'});
+            $('.startGame').text('Play Again?').css({'opacity': '1', 'z-index': '0', 'height': '70px'});
         }
     });
 });
