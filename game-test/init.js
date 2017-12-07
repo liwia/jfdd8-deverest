7c7c65f0d9a61a0930681f097a893b44401d4bae
Rafał Penkala <raf.tricity@gmail.com>
2017-12-07 16:39:18 +0100

    game v 1.1 working

--- game-test/init.js
+++ game-test/init.js
@@ -1,33 +1,35 @@
-var scoreNum = 0;
-
 $(document).ready(function () {
+    var scoreNum = 0;
     var bubble = $('.bubble');
+
     $('.startGame').click(function () {
         scoreNum = 0;
         $('.scoreBoard').css('visibility', 'visible');
         $('.timeBoard').css('visibility', 'visible');
         bubble.css('visibility', 'visible');
         $('.score').text(scoreNum);
-
+        var time = 30;
+        var interval = setInterval(function () {
+            if (time === 0) {
+                clearInterval(interval);
+                $('.startGame').text('Play Again?').css({'opacity': '1', 'z-index': '0', 'height': '70px'});
+                bubble.css('visibility', 'hidden');
+            }
+            $('.timer').text(time--)
+            ;
+        }, 1000);
+        var stopInterval = setInterval(function () {
+            var maxHeight = bubble.parent().height() - bubble.height();
+            var maxWidth = bubble.parent().width() - bubble.width();
+            bubble.css({
+                top: Math.random() * maxHeight,
+                left: Math.random() * maxWidth
+            });
+        }, 1000);
     });
-    var time = 30;
-    var interval = setInterval(function () {
-        if (time === 0) {
-            clearInterval(interval);
-            $('.startGame').text('Play Again?').css({'opacity': '1', 'z-index': '0', 'height': '70px'});
-            bubble.css('visibility', 'hidden');
-        }
-        $('.timer').text(time--)
-        ;
-    }, 1000);
-    var stopInterval = setInterval(function () {
-        var maxHeight = bubble.parent().height() - bubble.height();
-        var maxWidth = bubble.parent().width() - bubble.width();
-        bubble.css({
-            top: Math.random() * maxHeight,
-            left: Math.random() * maxWidth
-        });
-    }, 1000);
+
+
+
 
     bubble.click(function () {
         $('.score').text(scoreNum += 10);
