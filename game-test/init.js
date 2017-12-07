230c0368844f4747e802004bf0f978a9f2e658b4
Rafał Penkala <raf.tricity@gmail.com>
2017-12-07 16:23:30 +0100

    game v 1.0.3 + timer -still working on timer

--- game-test/init.js
+++ game-test/init.js
@@ -9,26 +9,25 @@ $(document).ready(function () {
         bubble.css('visibility', 'visible');
         $('.score').text(scoreNum);
 
-        var stopInterval = setInterval(function(){
-            var maxHeight = bubble.parent().height() - bubble.height();
-            var maxWidth = bubble.parent().width() - bubble.width();
-            bubble.css({
-                top: Math.random() * maxHeight,
-                left: Math.random() * maxWidth
-            });
-        }, 700);
     });
-        var time = 30;
-        var interval = setInterval(function () {
-            if (time === 0) {
-                clearInterval(interval);
-                $('.startGame').text('Play Again?').css({'opacity': '1', 'z-index': '0', 'height': '70px'});
-                bubble.css('visibility', 'hidden');
-            }
-            $('.timer').text(time--)
-            ;
-        }, 100);
-    });
+    var time = 30;
+    var interval = setInterval(function () {
+        if (time === 0) {
+            clearInterval(interval);
+            $('.startGame').text('Play Again?').css({'opacity': '1', 'z-index': '0', 'height': '70px'});
+            bubble.css('visibility', 'hidden');
+        }
+        $('.timer').text(time--)
+        ;
+    }, 1000);
+    var stopInterval = setInterval(function () {
+        var maxHeight = bubble.parent().height() - bubble.height();
+        var maxWidth = bubble.parent().width() - bubble.width();
+        bubble.css({
+            top: Math.random() * maxHeight,
+            left: Math.random() * maxWidth
+        });
+    }, 1000);
 
     bubble.click(function () {
         $('.score').text(scoreNum += 10);
@@ -36,6 +35,10 @@ $(document).ready(function () {
             bubble.css('visibility', 'hidden');
             $('.toWin').text('You Win!').css({'opacity': '1', 'z-index': '0'});
         }
+    });
+
 });
+
+
 
 
